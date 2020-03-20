a9adc1a63b9e6ee3f08ee94982508cf1f4ae6f3f
Javier Tafur <javtafur@gmail.com>
2020-03-20 17:29:18 +0100

    Fade In Fde Out Logo Done

---
+++ Tutorials/Assets/Scripts/FadeImage.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeImage : MonoBehaviour
+{
+    // the image you want to fade, assign in inspector
+    public Image img;
+
+    IEnumerator ImageFade(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+    }
+
+    public void OnButtonClick()
+    {
+        // fades the image out when you click
+        StartCoroutine(ImageFade(true));
+    }
+
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
