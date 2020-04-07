8b2a054bf57ae4f779b80b36ad32ac469a4c08d0
Markel <martxelo94@gmail.com>
2020-04-07 18:50:15 +0200

    abilities

---
+++ cs388_final_project/Assets/Scripts/MutantVirus.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MutantVirus : MonoBehaviour
+{
+    private Game game;
+
+    public float duration = 10.0f;
+    private float current_duration = 0.0f;
+
+    public float recoverFactor = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        game = FindObjectOfType<Game>();
+
+        foreach (Human h in game.humans) {
+            h.recover_time *= recoverFactor;
+        }
+
+        Debug.Log("Mutant Virus Started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        current_duration += Time.deltaTime;
+        if (current_duration > duration)
+            Destroy(this);
+    }
+
+    private void OnDestroy()
+    {
+        foreach (Human h in game.humans)
+        {
+            h.recover_time /= recoverFactor;
+        }
+        Debug.Log("Mutant Virus Ended");
+    }
+}
